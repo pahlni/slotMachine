a46059673bbfec1f8667521571e705389f8970df
Nicholas Pahl <pahlni@mail.gvsu.edu>
2017-06-23 16:04:07 -0400

    LCD started but not working

--- main.c
+++ main.c
@@ -55,17 +55,54 @@
 #include "driverlib.h"
 
 /* Standard Includes */
+#include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
 
 #include "msp.h"
 
 #include "buttonDriver.h"
+#include "LCDDriver.h"
+#include "ClockSystem.h"
+
+void ClkInit(void)
+{
+    /* Configuring pins for peripheral/crystal usage and LED for output */
+    MAP_GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_PJ,
+            GPIO_PIN3 | GPIO_PIN2, GPIO_PRIMARY_MODULE_FUNCTION);
+    MAP_GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0);
+
+    /* Just in case the user wants to use the getACLK, getMCLK, etc. functions,
+     * let's set the clock frequency in the code.
+     */
+    CS_setExternalClockSourceFrequency(32000,48000000);
+
+    /* Starting HFXT in non-bypass mode without a timeout. Before we start
+     * we have to change VCORE to 1 to support the 48MHz frequency */
+    MAP_PCM_setCoreVoltageLevel(PCM_VCORE1);
+    MAP_FlashCtl_setWaitState(FLASH_BANK0, 2);
+    MAP_FlashCtl_setWaitState(FLASH_BANK1, 2);
+    CS_startHFXT(false);
+
+    /* Initializing MCLK to HFXT (effectively 48MHz) */
+    MAP_CS_initClockSignal(CS_MCLK, CS_HFXTCLK_SELECT, CS_CLOCK_DIVIDER_1);
+}
 
 int main(void)
 {
     /* Stop Watchdog  */
     MAP_WDT_A_holdTimer();
+
+    //Clock_Init48MHz();
+    ClkInit();
+
+    uint32_t smclk = MAP_CS_getSMCLK();
+
+    P6->DIR |= BIT6;
+    P6->OUT |= BIT6;
+
+    Output_Init();
+    printf("hello world");
 
     buttonInit();
 
@@ -77,7 +114,7 @@ int main(void)
 
     while (1)
     {
-        if(isButtonPress())
+        if (isButtonPress())
         {
             clearButtonPress();
             MAP_GPIO_toggleOutputOnPin(GPIO_PORT_P4, GPIO_PIN5);
